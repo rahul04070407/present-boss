1d7f9fe5a7b48133cab87a8c1bbb70d4f07ca25d
rahul04070407 <rahulp@vareli.co.in>
2025-05-21 17:54:42 +0530

    header design

--- present-Boss/src/app/pages/header/header.component.ts
+++ present-Boss/src/app/pages/header/header.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('menuToggle') menuToggle!: ElementRef;
+  @ViewChild('offcanvasMenu') offcanvasMenu!: ElementRef;
+  @ViewChild('closeBtn') closeBtn!: ElementRef;
 
+  ngAfterViewInit() {
+    const menuBtn = this.menuToggle.nativeElement;
+    const offcanvas = this.offcanvasMenu.nativeElement;
+    const close = this.closeBtn.nativeElement;
+
+    menuBtn.addEventListener('click', () => {
+      offcanvas.classList.add('open');
+    });
+
+    close.addEventListener('click', () => {
+      offcanvas.classList.remove('open');
+    });
+
+    window.addEventListener('click', (e: MouseEvent) => {
+      if (!offcanvas.contains(e.target as Node) && !menuBtn.contains(e.target as Node)) {
+        offcanvas.classList.remove('open');
+      }
+    });
+  }
 }
