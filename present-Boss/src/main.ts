31b78cb7aae17295af186cc292d119471dfb5beb
rahul04070407 <rahulp@vareli.co.in>
2025-05-22 17:21:24 +0530

    add animation

--- present-Boss/src/main.ts
+++ present-Boss/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+AOS.init({
+  duration: 1200,
+  offset: 200,
+});
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
