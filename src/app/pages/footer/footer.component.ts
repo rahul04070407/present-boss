68ce4818030523d85f713cff86f4b58cf12c3a77
rahul04070407 <rahulp@vareli.co.in>
2025-05-30 11:23:37 +0530

    present boss footer change

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  email = 'info@vareli.co.in'
+  email = 'sleads@vareli.co.in'
 }
